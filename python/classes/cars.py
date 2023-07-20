55fdcc9541a3dc6171fa5ac2548c9f55020241af
MitchMD33 <brown.l.mitchell@outlook.com>
2023-07-20 14:34:24 +0800

    Add the following files and changes to pytho-practice.
    	.vscode/
    	python/classes/__pycache__/
    	python/classes/car.py
    	python/classes/electric_car.py
    	python/classes/electric_carII.py
    	python/classes/ice_cream_stand.py
    	python/classes/my_cay.py
    	python/classes/my_electric_car.py

--- python/classes/cars.py
+++ python/classes/cars.py
@@ -6,7 +6,7 @@ class Car:
     self.make = make
     self.model = model 
     self.year = year
-    self.odometer_reading = 0
+    self.odometer_reading = 6
   
   def get_descriptive_name(self):
     """Return a neatly formatted descriptive name"""
@@ -33,10 +33,10 @@ class Car:
 my_used_car = Car('subaru', 'outback', 2015)
 print(my_used_car.get_descriptive_name())
 
-my_used_car.update_odometer(23_500)
+my_used_car.update_odometer(23_800)
 my_used_car.read_odometer()
 
-my_used_car.increment_odometer(100)
+my_used_car.increment_odometer(500)
 my_used_car.read_odometer()
 
 my_new_car = Car('audi', 'a4', 2019)
