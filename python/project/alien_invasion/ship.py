2062aba86dbf7cbc0ba3bd48c40fefae138977ec
MitchMD33 <brown.l.mitchell@outlook.com>
2023-07-21 15:50:20 +0800

    Saving files

--- python/project/alien_invasion/ship.py
+++ python/project/alien_invasion/ship.py
@@ -7,12 +7,34 @@ class Ship:
     """Initialize the ship and set its starting position"""
     self.screen = ai_game.screen
     self.screen_rect = ai_game.screen.get_rect()
+    self.settings = ai_game.settings
+    self.settings = ai_game.settings
     
-    #Load the ship image adn get its rect.
+    #Load the ship image and get its rect.
     self.image = pygame.image.load('/home/mitchell/Desktop/repo/python-practice/python/project/alien_invasion/images/spaceship2.bmp')
+    self.image = pygame.transform.scale(self.image, (100, 80))
     self.rect = self.image.get_rect()
     #Start each new ship at the bottom center of the screen.
     self.rect.midbottom = self.screen_rect.midbottom
+    
+    #Store a decimal value of the ship's horizontal position.
+    self.x = float(self.rect.x)
+    
+    #Movement flags
+    self.moving_right = False 
+    self.moving_left = False
+    
+    
+  def update(self):
+    """Update the ship's position based on the movement flag"""
+    #Update the ship's x value, not the rect. 
+    if self.moving_right and self.rect.right < self.screen_rect.right:
+      self.x += self.settings.ship_speed
+    if self.moving_left and self.rect.left > 0:
+      self.x -= self.settings.ship_speed
+    
+    #Update rect object from self.x
+    self.rect.x = self.x
 
   def blitme(self):
     """Draw the ship at its current location."""
